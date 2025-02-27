fdff9fa6379b118e6dd316ae00f6c0689e0a5eb8
Leonid <mops707@gmail.com>
2025-02-27 13:57:54 +0300

    Add initial React application structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Golden Estate</h1>
+        <p>Добро пожаловать в мир элитной недвижимости</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
